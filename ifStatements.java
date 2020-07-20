1bec275e59ae81dead5e50bb2bb73a9d32fd901d
Seth Xeflide <sethxeflide@evexwater.com>
2020-07-20 17:06:43 -0600

    First Commit

---
+++ ifStatements.java
@@ -0,0 +1,16 @@
+public class ifStatement{
+   public static void main(String args[]){
+   int x=Integer.parseInt(args[0]);
+   //int y=Integer.parseInt(args[1]);  
+   
+   
+   
+   if (x<0){
+      x=-x;
+      
+     
+   }
+   
+   }
+
+}
